3ff22fccae56d618c0e7413c801022a1b058d5c7
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-02-15 12:08:01 +0200

    Update change_version.sh

--- change_version.sh
+++ change_version.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 systemctl stop chainflip-engine.service
 systemctl stop chainflip-node.service
-sed -i 's/v'"$1"'/'"$2"'/g'  /etc/systemd/system/chainflip-engine.service 
+sed -i 's/v'"$1"'/v'"$2"'/g'  /etc/systemd/system/chainflip-engine.service 
 sed -i 's/v'"$1"'/v'"$2"'/g'  /etc/systemd/system/chainflip-node.service
 sed -i 's/v'"$2"'/v'"$2"'/g' /etc/logrotate.d/chainflip
 systemctl daemon-reload
