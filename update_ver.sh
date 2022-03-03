541f31bec3fb7b5c284dadf67d2ca9d6ef23c32f
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-03-03 17:49:10 +0200

    Update update_ver.sh

--- update_ver.sh
+++ update_ver.sh
@@ -3,6 +3,7 @@ cd ~/chainflip
 rm chainflip.tar.gz
 wget https://github.com/chainflip-io/chainflip-bin/releases/download/v0.2.3-soundcheck-soundcheck/chainflip.tar.gz
 tar -xvf chainflip.tar.gz
+mv chainflip-v0.2.3-soundcheck chainflip-v0.2.3
 chmod -R +x chainflip-v0.2.3/*
 cp -r chainflip-v0.2.2/config chainflip-v0.2.3/
 systemctl stop chainflip-engine.service
