c45d0672642cb59ab54a7aef9388d4b711e8b9fd
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-05-25 12:01:24 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd ~/chainflip
 rm chainflip.tar.gz
-wget https://github.com/chainflip-io/chainflip-bin/releases/download/v0.2.1-soundcheck/chainflip.tar.gz
+wget https://github.com/chainflip-io/chainflip-bin/releases/download/v0.2.6-soundcheck/chainflip.tar.gz
 tar -xvf chainflip.tar.gz
-chmod -R +x chainflip-v0.2.1/*
-cp -r chainflip-v0.2.0/config chainflip-v0.2.1/
+chmod -R +x chainflip-v0.2.6/*
+cp -r chainflip-v0.2.0/config chainflip-v0.2.5/
 systemctl stop chainflip-engine.service
 systemctl stop chainflip-node.service
-sed -i 's/v0.2.0/v0.2.1/g'  /etc/systemd/system/chainflip-engine.service 
-sed -i 's/v0.2.0/v0.2.1/g'  /etc/systemd/system/chainflip-node.service
-sed -i 's/v0.2.0/v0.2.1/g' /etc/logrotate.d/chainflip
+sed -i 's/v0.2.5/v0.2.6/g'  /etc/systemd/system/chainflip-engine.service 
+sed -i 's/v0.2.5/v0.2.6/g'  /etc/systemd/system/chainflip-node.service
+sed -i 's/v0.2.5/v0.2.6/g' /etc/logrotate.d/chainflip
 systemctl daemon-reload
 systemctl start chainflip-engine
 systemctl start chainflip-node
