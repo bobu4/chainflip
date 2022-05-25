65748e56504282e56a1ae44b418c1bd79201ba65
bobu4 <48527047+bobu4@users.noreply.github.com>
2022-05-25 12:18:44 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,12 +4,11 @@ rm chainflip.tar.gz
 wget https://github.com/chainflip-io/chainflip-bin/releases/download/v0.2.6-soundcheck/chainflip.tar.gz
 tar -xvf chainflip.tar.gz
 chmod -R +x chainflip-v0.2.6/*
-cp -r chainflip-v0.2.0/config chainflip-v0.2.5/
+cp -r chainflip-v0.2.5/config chainflip-v0.2.6/
 systemctl stop chainflip-engine.service
 systemctl stop chainflip-node.service
 sed -i 's/v0.2.5/v0.2.6/g'  /etc/systemd/system/chainflip-engine.service 
 sed -i 's/v0.2.5/v0.2.6/g'  /etc/systemd/system/chainflip-node.service
 sed -i 's/v0.2.5/v0.2.6/g' /etc/logrotate.d/chainflip
-systemctl daemon-reload
-systemctl start chainflip-engine
-systemctl start chainflip-node
+sed -i 's/chaindata/chaindata-two/g' /etc/systemd/system/chainflip-node.service
+sed -i 's/--chain soundcheck/--chain soundcheck-two/g' /etc/systemd/system/chainflip-node.service
